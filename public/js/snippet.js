3467608abbbee8719e7f061c5ab53fcc5c8e3443
mewdew <shivangisinha612@gmail.com>
2016-11-26 20:25:59 +0530

    Data reflecting in cards now

--- public/js/snippet.js
+++ public/js/snippet.js
@@ -1,117 +1,37 @@
-
-
-
 $("#fet").click(function(){
   $.get('http://localhost:8000/fetchdata', {}, function(data){
 
-        
-
-        // for (var i = 1, i<6; i++) {
-        // 	var qid = "q"+i;
-        // 	var q   =  0;
-
-        // 	$(qid).html(data[0]);
-
-
-        // }
-
-        var loadSnippet = function (selector,snippet) {
-        	var q = 0;
-        	$("#"+selector).load(snippet);
-        	console.log(data[q]);
-        	$("#q").html(data[q]);
-        	q = q+3;
-
-         };
-
-
-     
       $(document).ready(function(){
 
+        /* Function to render Data on qCards */
+        function renderQcards(indx, dIndx){
+          if(indx > 5)
+            return;
+          var selectid = "qabox" + indx;
+          var ques = data[dIndx];
+          $("#"+selectid).load( "cardsnippet.html", function(){
+            /* Question for each card from data obtained */
+            $(this).find(".q").html("<h4><b>" + data[dIndx] + "</b></h4>"); 
+            /* Answer for the above rendered question */
+            $(this).find(".ansBlock").html(data[dIndx+1]);
+            /* User who answered the question */
+            $(this).find(".uName").html( " " + data[dIndx+2] );
+            renderQcards(indx+1, dIndx+3);
+           });
+        }
 
-      	 for (var i = 1, q= 0; i<6; i++) {
-		
-		var selectId = "qabox" + i;
-		var snippet = "cardsnippet.html";
-		console.log(selectId);
-
-		loadSnippet(selectId,snippet);
-
-        console.log( "Load was performed." );
-
-    }
+        renderQcards(1,0);
 
 
         for (var i = 1; i<4; i++) {
-		
-		var selectId = "invitebox" + i;
-		$("#"+selectId).load("invitesnippet.html");
-        console.log( "Load was performed." );
+    
+          var selectId = "invitebox" + i;
+          $("#"+selectId).load("invitesnippet.html");
 
-    }
+        }
 
     });
 
-
-
-
- 
-
-      
-
-    // $(document).ready(function(){
-    //     (function implRendering($){
-
-    //     	console.log(indx);
-    //     	if(indx < 6){
-    //     		console.log("here");
-    //     		var selectId = "qabox" + indx;
-	   //      	$("#"+selectId).load("cardsnippet.html",qNo, function(){
-	   //      	  $("#q").html(data[qNo]);
-	   //      	  implRendering(0, qNo );
-	   //      	  console.log("Qno: "+ qNo);
-		  //       });
-    //     	}
-    //     })(0,0);
-
-    // });
-
-  //       $(document).ready(function(){
-
-		// for (var i = 1, q = 0 ; i<6; i++, q=q+3) {
-		// 	// (function (i,q){
-		// 		var selectid = "qabox" + i;
-
-  //      $("#"+selectid).load( "cardsnippet.html", function(res, status, xhr){
-  //      	  var response = res;
-  //      	  $("#q").html(response);
-  //      });
-
-  //    }
-
-  //       for (var i = 1; i<4; i++) {
-		
-		// var selectid = "invitebox" + i;
-		// console.log(selectid);
-
-		// $("#"+selectid).load( "invitesnippet.html", function() {
-  //       console.log( "Load was performed." );
-  //   });
-
-      
-     
-  //    }  
-
-  //   }); 
-
-
-
 });
 
 });
-
-
-
-
-
-
